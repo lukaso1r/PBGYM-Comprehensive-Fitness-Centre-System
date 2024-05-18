31ba236e5399fe8efa86ff0ad83abc7de95cccea
Łukasz Kaliszewicz <lukasz.kaliszewicz01@gmail.com>
2024-05-18 23:43:37 +0200

    feat(base): setup basic lib and home page

---
+++ components/header-web.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  <header class="flex items-center justify-between px-16 py-4 bg-transparent text-neutral-700 absolute top-0 w-full z-99">
+    <!-- Logo -->
+    <div class="flex items-center space-x-2">
+      <img src="/images/header/logo.svg" alt="Logo" class="w-40" />
+    </div>
+
+    <!-- Menu -->
+    <nav class="hidden md:flex space-x-20 ">
+      <NuxtLink to="/o-nas" class="text-neutral-700 hover:text-gray-300">O nas</NuxtLink>
+      <NuxtLink to="/treningi" class="text-neutral-700 hover:text-gray-300">Treningi</NuxtLink>
+      <NuxtLink to="/zajecia-grupowe" class="text-neutral-700 hover:text-gray-300">Zajęcia Grupowe</NuxtLink>
+      <NuxtLink to="/blog" class="text-neutral-700 hover:text-gray-300">Blog</NuxtLink>
+      <NuxtLink to="/cennik" class="text-neutral-700 hover:text-gray-300">Cennik</NuxtLink>
+    </nav>
+
+    <!-- Button -->
+    <div>
+      <UButton class="btn-gradient text-white font-bold py-2 px-4 rounded">
+        Zaloguj się lub załóż konto
+      </UButton>
+    </div>
+  </header>
+</template>
+
+
+<style scoped>
+/* Additional styles if needed */
+    .btn-gradient{
+        background: linear-gradient(45deg, #2878FF, #2C65C8);
+    }
+    .btn-gradient:hover{
+        background: linear-gradient(20deg, #2C65C8, #2878FF);
+        box-shadow:0px 0px 5px -2px rgba(66, 68, 90, 1);
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <header-web />
+
+    <img src="/public/images/home-banner/blob1.svg" alt="" class="absolute left-0 top-0 -z-50">
+    <img src="/public/images/home-banner/blob2.svg" alt="" class="absolute right-0 bottom-0 -z-50">
+    <img src="/public/images/home-banner/blob3.svg" alt="" class="absolute top-[20%] left-[40%] -z-50">
+    <img src="/public/images/home-banner/blob4.svg" alt="" class="absolute bottom-0 left-0 -z-50">
+
+    <div class=" flex flex-row z-99 w-[90%] mx-auto mt-[6%] justify-center items-center">
+        <div class="col1">
+            <h1 class="font-bold text-4xl">Miejsce gdzie Twoje cele stają się rzeczywistością!</h1>
+            <h2>Nasza misja to nie tylko budowanie sylwetki, ale również kształtowanie zdrowego stylu życia i pozytywnego nastawienia. </h2>
+            <div class="btn-container">
+                <UButton label="Zobacz film o naszej siłowni"/>
+                <UButton label="Dowiedz się więcej"/>
+            </div>
+        </div>
+
+        <div class="col2">
+            <img src="/public/images/home-banner/Group 27.png" alt="" class="z-99 w-[100%]" >
+        </div>
+    </div>
+    
+
+
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+
+<style scoped>
+
+    body{
+        width: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    
+
+</style>
