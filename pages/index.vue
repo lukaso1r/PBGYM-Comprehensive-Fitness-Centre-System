c75c32d33f25c0819f901d4e233ea37eb02319ab
Łukasz Kaliszewicz <lukasz.kaliszewicz01@gmail.com>
2024-05-20 22:49:37 +0200

    feat(home-page) add footer component

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
             <p>Nasza misja to nie tylko budowanie sylwetki, ale również kształtowanie zdrowego stylu życia i pozytywnego nastawienia. </p>
             <div class="btn-container flex flex-row gap-4">
                 <UButton label="Zobacz film o naszej siłowni" class="bg-[#f1f6ff] text-lg text-[#2878FF] font-bold px-6 py-2 hover:bg-[#bed5ff]" icon="i-ic-round-play-arrow" />
-                <UButton label="Dowiedz się więcej" class="bg-[#2878FF] text-lg text-[#f1f6ff] font-bold px-6 py-2 hover:bg-[#2060cc]" icon="i-material-symbols-123"/>
+                <UButton label="Dowiedz się więcej" class="bg-[#2878FF] text-lg text-[#f1f6ff] font-bold px-6 py-2 hover:bg-[#2060cc]"/>
             </div>
         </div>
 
@@ -263,41 +263,7 @@
         </div>
     </div>
 
-    <footer class="text-white flex flex-row justify-between w-[100%] px-[10%] py-12 align-middle bg-[#001333] z-10" >
-        <img src="/public/images/logo-white.svg" alt="">
-        <div class="col1 flex flex-col justify-center">
-            <h5 class="text-2xl font-bold pb-3 ">O nas</h5>
-            <ul>
-                <li>Historia</li>
-                <li>Kompleks</li>
-                <li>Galeria</li>
-            </ul>
-        </div>
-        <div class="col2 flex flex-col justify-center">
-            <h5 class="text-2xl font-bold pb-3 ">Specyfikacja</h5>
-            <ul>
-                <li>Siłownia</li>
-                <li>Treningi</li>
-                <li>Zajęcia grupowe</li>
-            </ul>
-        </div>
-        <div class="col3  flex flex-col justify-center">
-            <h5 class="text-2xl font-bold pb-3 ">Social Media</h5>
-            <ul>
-                <li>Instagram</li>
-                <li>Facebook</li>
-                <li>Youtube</li>
-            </ul>
-        </div>
-        <div class="col4 flex flex-col justify-center">
-            <h5 class="text-2xl font-bold pb-3 ">Kontakt</h5>
-            <ul class="">
-                <li>Jabłonowa 11, Białystok</li>
-                <li>hej@pbgym.pl</li>
-                <li>tel: 123 654 879</li>
-            </ul>
-        </div>
-    </footer>
+    <footer-web/>
     
 
     
