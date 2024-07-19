85ae07ee233bbe3fefb993e0f172865653f6617f
Łukasz Kaliszewicz <lukasz.kaliszewicz01@gmail.com>
2024-07-19 23:51:14 +0200

    feat(navbar): code navbar desing

--- components/user-profile-navbar.vue
+++ components/user-profile-navbar.vue
@@ -6,24 +6,34 @@ const items = [
         icon: 'i-ic-round-local-offer',
         defaultOpen: false,
         slot: 'checkoffer',
+        color: 'orange',
+        class: 'py-4 hover:bg-orange-100',
+        
     },
     {
         label: 'Zajęcia',
         icon: 'i-ic-outline-sports-kabaddi',
         defaultOpen: false,
         slot: 'checkoffer',
+        color: 'cyan',
+        class: 'py-4 hover:bg-cyan-100'
     },
     {
         label: 'Trenerzy',
         icon: 'i-ic-baseline-emoji-people',
         defaultOpen: false,
         slot: 'checkoffer',
+        color: 'green',
+        class: 'py-4 hover:bg-green-100'
     },
     {
         label: 'Dokumenty',
         icon: 'i-ic-round-collections-bookmark',
         defaultOpen: false,
         slot: 'checkoffer',
+        color: 'red',
+        variant: 'soft',
+        class: 'py-4 hover:bg-red-100'
     }
 ]
 
@@ -32,7 +42,12 @@ const instructions = [
         label: 'Instrukcje',
         icon: 'i-ic-baseline-integration-instructions',
         defaultOpen: false,
-        slot: 'insctruction'
+        slot: 'insctruction',
+        color:'purple',
+        variant: 'soft',
+        content: 'Lorem ipsum dolor sit amet',
+        class: 'py-4 hover:bg-purple-100 '
+        
     }
 ]
 
@@ -41,33 +56,38 @@ const instructions = [
 <template>
 
     <nav class="px-6 -mt-[5%]">
-        <div class="bg-white px-4 rounded-2xl">
-            <NuxtLink to="/twoj-profil"><img class="max-w-full" src="/images/logo-blue.svg"/></NuxtLink>
+        <div class="bg-white px-4 py-4 rounded-2xl flex flex-col gap-y-3 ">
+            <NuxtLink to="/twoj-profil"><img class="max-w-[80%] my-0 mx-auto " src="/images/logo-blue.svg"/></NuxtLink>
             <hr/>
-            <UButton
-                class="bg-[#203983] hover:bg-[#617F9B]"
-                to="/twoj-profil"
-                icon="i-ic-sharp-home"
-            >
-                Kokpit
-            </UButton>
-            <UAccordion :items="items" color="white">
-                <template #checkoffer >
-                    <div>
-                        <p>Sprawdź ofertę!</p>
+            <div class="navigationContainer flex flex-col gap-y-0">
+                <UButton  class="w-full py-4 " to="/twoj-profil" icon="i-ic-sharp-home" variant="ghost" color="blue">
+                    Kokpit
+                </UButton>
+                <!-- TODO: change text color to -->
+                <UAccordion :items="items" size="sm" >
+                    <template #checkoffer>
+                        <div >
+                            <p>Sprawdź ofertę!</p>
+                        </div>
+                    </template>
+                </UAccordion>  
+                <hr/>
+                <UAccordion :items="instructions" color="primary"
+                    variant="soft"
+                    size="sm"
+                    class="my-2"
+                    >
+                    <template #insctruction>
+                        <div>
+                            <p>Tu instrukcje</p>
 
-                    </div>
-                </template>
-            </UAccordion>  
-            <hr/>
-            <UAccordion :items="instructions" color="white">
-                <template #insctruction>
-                    <div>
-                        <p>Tu instrukcje</p>
-
-                    </div>
-                </template>
-            </UAccordion>  
+                        </div>
+                    </template>
+                </UAccordion>  
+                
+            </div>
+            
+            
             
 
         </div>
@@ -77,5 +97,7 @@ const instructions = [
 
 
 <style scoped>
+    /*TODO: Due to delays, some of the code was written in plain CSS rather than using correct Tailwind practices.  It gonna be changed later*/
     
+  
 </style>
--- pages/twoj-profil.vue
+++ pages/twoj-profil.vue
@@ -6,7 +6,7 @@
 <template>
     <header-user-profile></header-user-profile>
     <div class="flex bg-[#F5F7F8]">
-        <user-profile-navbar class="basis-1/5"></user-profile-navbar>
+        <user-profile-navbar class="basis-1/5 max-w-[350px]"></user-profile-navbar>
         <main class=" min-h-svh basis-4/5">
             content
         </main>
