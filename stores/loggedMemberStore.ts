cfa5c1f496a82faea55b9e5b445d6995d91393a6
Łukasz Kaliszewicz <lukasz.kaliszewicz01@gmail.com>
2024-09-19 18:30:27 +0200

    loginMemberStore

---
+++ stores/loggedMemberStore.ts
@@ -0,0 +1,13 @@
+import type {  } from "~/types";
+
+export const useLoggedMemberStore = defineStore('loggedMemberStore', () => {
+    
+    const doSomething = () => {
+        return 'soon'
+    }
+
+
+    return {
+        doSomething
+    }
+});
