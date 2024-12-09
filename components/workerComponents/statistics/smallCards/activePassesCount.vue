80c720a155e87c3cd30dbfae0a328b2c9b10205b
Łukasz Kaliszewicz <lukasz.kaliszewicz01@gmail.com>
2024-12-09 14:30:03 +0100

    fyx

--- components/workerComponents/statistics/smallCards/activePassesCount.vue
+++ components/workerComponents/statistics/smallCards/activePassesCount.vue
@@ -14,7 +14,7 @@ onMounted(() => {
 
     <div class="activePassesCount smallStatsCard blockCustom col-span-1 grid grid-cols-2 items-center justify-between">
         <div class="col-span-1">
-            <p class=" text-sm font-semibold tracking-wide pb-1">Ilość aktywnych karnetów </p>
+            <p class="text-gray-500 text-sm font-semibold tracking-wide pb-1">Ilość aktywnych karnetów </p>
             <div v-if="statisticStore.activePassCount!=null" class="font-bold text-lg">
                 {{ statisticStore.activePassCount }}
             </div>                    
