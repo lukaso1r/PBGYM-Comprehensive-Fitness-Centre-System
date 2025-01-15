ab9011e4d521300896f05b52464d6657dd4cce41
Łukasz Kaliszewicz <48961301+lukaso1r@users.noreply.github.com>
2025-01-15 09:35:11 +0100

    Update footer-web.vue

--- components/footer-web.vue
+++ components/footer-web.vue
@@ -1,20 +1,18 @@
 <template>
     <footer class="text-white flex flex-row justify-between w-[100%] px-[10%] py-12 align-middle bg-[#001333] z-10" >
         <img src="/public/images/logo-white.svg" alt="">
+        <div class="col2 flex flex-col justify-center">
+            <h5 class="text-2xl font-bold pb-3 ">Oferta</h5>
+            <ul>
+                <li><NuxtLink to="/trenerzy-personalni">Trenerzy personalni</NuxtLink></li>
+                <li><NuxtLink to="/zajecia-grupowe">Zajęcia grupowe</NuxtLink></li>
+                <li><NuxtLink to="/#cennik">Cennik</NuxtLink></li>
+            </ul>
+        </div>
         <div class="col1 flex flex-col justify-center">
             <h5 class="text-2xl font-bold pb-3 ">O nas</h5>
             <ul>
-                <li>Historia</li>
-                <li>Kompleks</li>
-                <li>Galeria</li>
-            </ul>
-        </div>
-        <div class="col2 flex flex-col justify-center">
-            <h5 class="text-2xl font-bold pb-3 ">Specyfikacja</h5>
-            <ul>
-                <li>Siłownia</li>
-                <li>Treningi</li>
-                <li>Zajęcia grupowe</li>
+                <li><NuxtLink to="/wpisy">Aktualności</NuxtLink></li>
             </ul>
         </div>
         <div class="col3  flex flex-col justify-center">
